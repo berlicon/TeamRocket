1ed1e916ecf091d3392f8a1f1576b06fdda26435
Dmitry <dgurov@inbox.ru>
2020-02-08 19:37:58 +0300

    hide token

--- NeuralNet/speach_recognizer.py
+++ NeuralNet/speach_recognizer.py
@@ -2,7 +2,7 @@ import os
 from pydub import AudioSegment
 
 
-def convert(root_voice, format_str):
+def convert(root_voice : str, format_str : str) -> str:
     for vfn in os.listdir(root_voice):
         vfnp = vfn.split('.')
         if vfnp[-1] == format_str:
@@ -11,8 +11,12 @@ def convert(root_voice, format_str):
                 vm4a.export(voggf, format="ogg")
 
 
+def read_token(f_path: str) -> str:
+    with open(f_path, "r") as f:
+        token = f.read().replace('\n', '')
+    return token
+
+
 if __name__ == "__main__":
-    convert("test_voice/", "m4a")
-
-
-oauth_token = "AgAAAAAOOjGmAATuwRo8IAGkZE6Qj85CimQnKY4"
+    # convert("test_voice/", "m4a")
+    print(read_token("token.txt"))
