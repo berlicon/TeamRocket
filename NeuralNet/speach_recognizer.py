2c50dc4cda52d8763546352768025220e16b2e86
Dmitry <dgurov@inbox.ru>
2020-02-09 00:31:58 +0300

    Yandex API

--- NeuralNet/speach_recognizer.py
+++ NeuralNet/speach_recognizer.py
@@ -1,8 +1,10 @@
 import os
+import json
+import requests
 from pydub import AudioSegment
 
 
-def convert(root_voice : str, format_str : str) -> str:
+def convert(root_voice: str, format_str: str) -> str:
     for vfn in os.listdir(root_voice):
         vfnp = vfn.split('.')
         if vfnp[-1] == format_str:
@@ -11,12 +13,43 @@ def convert(root_voice : str, format_str : str) -> str:
                 vm4a.export(voggf, format="ogg")
 
 
-def read_token(f_path: str) -> str:
+def read_secret(f_path: str) -> str:
     with open(f_path, "r") as f:
-        token = f.read().replace('\n', '')
-    return token
+        secret = f.read().replace('\n', '')
+    return secret
+
+
+def get_iam_token() -> str:
+    oauth_token = read_secret("token_oauth.txt")
+    url = "https://iam.api.cloud.yandex.net/iam/v1/tokens"
+    data = {"yandexPassportOauthToken": oauth_token}
+    answer = requests.post(url, json=data)
+    return answer.json()['iamToken']
+
+
+def recognize(vocie_file) -> str:
+    folder_id = read_secret("folder_id.txt")
+    url = f"https://stt.api.cloud.yandex.net/speech/v1/stt:recognize?folderId={folder_id}"
+    answer = requests.post(url, headers={"Authorization": f"Bearer {get_iam_token()}"}, data=vocie_file)
+
+    if answer.status_code != 200:
+        return "Ошибка распознавания речи. Попробуйте ввести запрос на клавиатуре."
+    else:
+        return answer.json()["result"]
+
+
+def get_files(root_voice: str, format_str: str):
+    varr = []
+    for vfn in os.listdir(root_voice):
+        vfnp = vfn.split('.')
+        if vfnp[-1] == format_str:
+            with open(root_voice + vfn, "rb") as vf:
+                varr.append(vf.read())
+    return varr
 
 
 if __name__ == "__main__":
     # convert("test_voice/", "m4a")
-    print(read_token("token.txt"))
+
+    for vfile in get_files("test_voice/", "ogg"):
+        print(recognize(vfile))
